1a5715b3a92f87bd7ccdcc0b79784c33e3130214
Nico De Castro <jtdecastro@up.edu.ph>
2024-05-26 19:47:03 +0800

    refactor: group concat estab contact in queries api

--- QueriesAPI.py
+++ QueriesAPI.py
@@ -13,19 +13,19 @@ class QueriesAPI():
 
     # ----- SELECT STATEMENTS -----
     def select_all_food_estabs(self):
-        sql_statement = "SELECT estab_id, estab_desc, estab_name, contact, loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE GROUP BY estab_id"
+        sql_statement = "SELECT estab_id, estab_desc, estab_name, GROUP_CONCAT(contact), loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE GROUP BY estab_id"
         self.cursor.execute(sql_statement)
         result = self.cursor.fetchall()
         return result
     
     def select_food_estab_by_id(self, id):
         if(id == ''):
-            sql_statement = "SELECT estab_id, estab_desc, estab_name, contact, loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE GROUP BY estab_id"
+            sql_statement = "SELECT estab_id, estab_desc, estab_name, GROUP_CONCAT(contact), loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE GROUP BY estab_id"
         elif(bool(re.search(r'[^\d]', id))):
             print('contains non-numeric chars')
             return []
         else:
-            sql_statement = f"SELECT estab_id, estab_desc, estab_name, contact, loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE WHERE estab_id={id} GROUP BY estab_id"
+            sql_statement = f"SELECT estab_id, estab_desc, estab_name, GROUP_CONCAT(contact), loc, GROUP_CONCAT(serv_mod) FROM FOOD_ESTABLISHMENT NATURAL JOIN FOOD_ESTABLISHMENT_CONTACT NATURAL JOIN FOOD_ESTABLISHMENT_LOCATION NATURAL JOIN FOOD_ESTABLISHMENT_MODE_OF_SERVICE WHERE estab_id={id} GROUP BY estab_id"
         self.cursor.execute(sql_statement)
         result = self.cursor.fetchall()
         return result
