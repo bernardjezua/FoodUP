6025bd8dc9b6cee75e3cb6a0e48682ade91c00cb
koolano <koolano@up.edu.ph>
2024-06-03 23:30:15 +0800

    ui: Fixed overflowing string values in tables to wrappable.

--- ViewReview.py
+++ ViewReview.py
@@ -1,3 +1,4 @@
+from functools import partial
 from pathlib import Path
 import re, sys, subprocess
 
@@ -176,6 +177,47 @@ table.place(
     width=494,
     height=262
 )
+
+style = ttk.Style()
+style.configure('Treeview', rowheight=45)
+def motion_handler(tree, event):
+    f = font.Font(font='TkDefaultFont')
+    # A helper function that will wrap a given value based on column width
+    def adjust_newlines(val, width, pad=10):
+        if not isinstance(val, str):
+            return val
+        else:
+            words = val.split()
+            lines = [[],]
+            for word in words:
+                line = lines[-1] + [word,]
+                if f.measure(' '.join(line)) < (width - pad):
+                    lines[-1].append(word)
+                else:
+                    lines[-1] = ' '.join(lines[-1])
+                    lines.append([word,])
+
+            if isinstance(lines[-1], list):
+                lines[-1] = ' '.join(lines[-1])
+
+            return '\n'.join(lines)
+
+    if (event is None) or (tree.identify_region(event.x, event.y) == "separator"):
+        # You may be able to use this to only adjust the two columns that you care about
+        # print(tree.identify_column(event.x))
+
+        col_widths = [tree.column(cid)['width'] for cid in tree['columns']]
+
+        for iid in tree.get_children():
+            new_vals = []
+            for (v,w) in zip(tree.item(iid)['values'], col_widths):
+                new_vals.append(adjust_newlines(v, w))
+            tree.item(iid, values=new_vals)
+
+
+table.bind('<B1-Motion>', partial(motion_handler, table))
+motion_handler(table, None) 
+
 
 db = QueriesAPI()
 result = db.select_all_food_reviews()
